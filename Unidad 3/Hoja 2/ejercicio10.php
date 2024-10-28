08ed63e1f4223a30cf70eb30774362a774e8105f
Guteeh-GG <mariogg98@hotmail.com>
2024-10-28 16:40:18 +0100

    diana a medias

--- Unidad 3/Hoja 2/ejercicio10.php
+++ Unidad 3/Hoja 2/ejercicio10.php
@@ -1,11 +1,44 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio10</title>
+    <title>Diana con Disparos Aleatorios</title>
+    <style>
+
+        .circle {
+            border-radius: 50%;
+            position: absolute;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+
+        .diana {
+            position: relative;
+            width: 400px;
+            height: 400px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+    </style>
 </head>
 <body>
-    
-</body>
-</html>
+
+<div class="diana">
+    <?php
+    $numcirculos = 10; 
+    $maxtamanio = 300;
+    $color1 = 'red'; 
+    $color2 = 'white'; 
+
+    for ($i = 0; $i < $numcirculos; $i++) {
+        $tamanio = $maxtamanio - $i * ($maxtamanio / $numcirculos);
+        $color = ($i % 2 == 0) ? $color1 : $color2; 
+        $puntdiana = $numcirculos -$i;
+        echo "<div class='circle' style='width: {$tamanio}px; height: {$tamanio}px; background-color: {$color};'>$puntdiana</div>";
+    }
+
+    ?>
