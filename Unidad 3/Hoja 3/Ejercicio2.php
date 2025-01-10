9707be5120d9709c00e691c78bfb8264a1cf2f5a
Guteeh-GG <mariogg98@hotmail.com>
2025-01-10 17:19:45 +0100

    2

--- Unidad 3/Hoja 3/Ejercicio2.php
+++ Unidad 3/Hoja 3/Ejercicio2.php
@@ -10,17 +10,31 @@
 <?php
 
 // Escriba un programa que muestre un animal al azar y su nombre.
+
 // Haz este ejercicio de dos formas:
 // 3.1. Con dos matrices de una dimensión:
 // Cree dos matrices, una con los nombres de los ficheros ("ballena.png",
 // "caballito-mar.png", etc.) y otra con los nombres de los animales ("Ballena",
 // "Caballito de mar", etc.), en el mismo orden. Elija un valor del índice al azar para
 // mostrar tanto el dibujo como el texto.
+
+$animals = array("ballena.jpg", "caballito-de-mar.jpg", "conejo.jpg", "elefante.jpg", "giraffe.jpg");
+$nombresAnimales = array("Ballena", "Caballito de mar", "Conejo", "Elefante", "Giraffe");
+
+$index = rand(0, count($animals) - 1);
+
+echo "<img src='/Unidad 3/Hoja 3/animales{$animals[$index]}.jpg'>";
+
+echo "<br>{$nombresAnimales[$index]}";
+
 // 3.2. Con una matriz de dos dimensiones:
 // Cree una matriz en la que cada elemento sea una matriz con el nombre del animal
 // ("Ballena", "Caballito de mar", etc.) y el nombre del fichero ("ballena.png",
 // "caballito-mar.png", etc.) como elementos. Elija un valor del índice al azar para
 // mostrar tanto el dibujo como el texto.
+
+
+
 
 ?>
 
